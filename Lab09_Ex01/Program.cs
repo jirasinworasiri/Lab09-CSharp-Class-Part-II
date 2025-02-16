71844f47b6d5ec4d80534339f8deaebc138bf6da
Salinatooninter <66030284@kmitl.ac.th>
2025-02-16 21:11:46 +0700

    update lab09

---
+++ Lab09_Ex01/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Person.name = "jirasin";  
+System.Console.WriteLine(Person.GetName());  
+
+static class Person
+{
+    public static string? name;  
+    
+    public static string GetName()  
+    {
+        return $"Hello from {name}";  
+    }
+}
---
+++ Lab09_Ex03/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Person.name = "jirasin"; 
+System.Console.WriteLine(Person.GetName());  
+var student = new Student();
+Student.name = "66030029";
+Console.WriteLine(Student.GetName());
+
+class Person
+{
+    public static string? name;  
+    public static string GetName()
+    {
+        return $"Hello from {name}";
+    }
+}
+
+class Student : Person
+{
+    
+}
